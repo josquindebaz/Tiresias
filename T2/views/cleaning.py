f6c7f676fea1375f983540a4a9d0dfd994fafc0d
josquindebaz <josquin.debaz@gmail.com>
2019-05-22 19:28:59 +0200

    pylint partial

--- T2/views/cleaning.py
+++ T2/views/cleaning.py
@@ -1,3 +1,8 @@
+"""Viewer for cleaning module
+Josquin Debaz
+GPL3
+"""
+
 import tkinter as tk
 from tkinter import ttk
 from tkinter import filedialog
@@ -8,89 +13,106 @@ from mod.cleaning import list_files
 from mod.cleaning import Cleaner
 
 class ViewCleaning():
+    """cleaning window"""
     def __init__(self, parent):
         self.parent = parent
-        WindowTitle = tk.Label(parent, text="Character Cleaning",
-            font=("Helvetica", 12, "bold"))
-        WindowTitle.pack(fill=tk.X)
+        windowtitle = tk.Label(parent,
+                               text="Character Cleaning",
+                               font=("Helvetica", 12, "bold"))
+        windowtitle.pack(fill=tk.X)
 
         #Frame directory
-        Fr1 = tk.Frame(parent)
-        Fr1.pack(anchor=tk.W)
+        fr1 = tk.Frame(parent)
+        fr1.pack(anchor=tk.W)
 
-        bnDir = tk.Button(Fr1, text=u"Select directory",
-            command=self.sel_dir)
-        bnDir.pack(side=tk.LEFT)
+        bndir = tk.Button(fr1,
+                          text="Select directory",
+                          command=self.sel_dir)
+        bndir.pack(side=tk.LEFT)
 
-        self.choosenDir = tk.StringVar()
-        dir_entry = tk.Entry(Fr1, width=52,
-            textvariable=self.choosenDir)
-        self.choosenDir.set(u"C:\corpus")
+        self.choosendir = tk.StringVar()
+        dir_entry = tk.Entry(fr1,
+                             width=52,
+                             textvariable=self.choosendir)
+        self.choosendir.set(r"C:\corpus")
         dir_entry.pack(side=tk.LEFT)
 
-        self.Recursive = tk.BooleanVar()
-        bnRecursive = tk.Checkbutton(Fr1, 
-            text='recursive', var=self.Recursive)
-        bnRecursive.select()
-        bnRecursive.pack(side=tk.LEFT)
+        self.is_recursive = tk.BooleanVar()
+        bnrecursive = tk.Checkbutton(fr1,
+                                     text='recursive',
+                                     var=self.is_recursive)
+        bnrecursive.select()
+        bnrecursive.pack(side=tk.LEFT)
 
         self.test = tk.BooleanVar()
-        bn_test = tk.Checkbutton(Fr1, 
-            text='test only', var=self.test)
+        bn_test = tk.Checkbutton(fr1,
+                                 text='test only',
+                                 var=self.test)
         #bn_test.select()
         bn_test.pack(side=tk.LEFT)
 
-        bnAction = tk.Button(Fr1, text=u"Process cleaning",
-                             command=self._t_Action)
-        bnAction.pack(side=tk.LEFT)
+        bnaction = tk.Button(fr1,
+                             text="Process cleaning",
+                             command=self.t_action)
+        bnaction.pack(side=tk.LEFT)
 
         #Frame Options
-        Fr2 = tk.LabelFrame(parent, 
-            text="Options", borderwidth=1)
-        Fr2.pack()
+        fr2 = tk.LabelFrame(parent,
+                            text="Options",
+                            borderwidth=1)
+        fr2.pack()
         self.utf = tk.BooleanVar()
-        bn_utf = tk.Checkbutton(Fr2,
-            text='Utf8 to Latin1', var=self.utf)
+        bn_utf = tk.Checkbutton(fr2,
+                                text='Utf8 to Latin1',
+                                var=self.utf)
         bn_utf.select()
-        bn_utf.pack(side=tk.LEFT)        
+        bn_utf.pack(side=tk.LEFT)
         self.ascii = tk.BooleanVar()
-        bn_ascii = tk.Checkbutton(Fr2,
-            text='ascii', var=self.ascii)
+        bn_ascii = tk.Checkbutton(fr2,
+                                  text='ascii',
+                                  var=self.ascii)
         bn_ascii.select()
         bn_ascii.pack(side=tk.LEFT)
         self.char_replace = tk.BooleanVar()
-        bn_char_replace = tk.Checkbutton(Fr2,
-            text='special chars', var=self.char_replace)
+        bn_char_replace = tk.Checkbutton(fr2,
+                                         text='special chars',
+                                         var=self.char_replace)
         bn_char_replace.select()
-        bn_char_replace.pack(side=tk.LEFT)  
+        bn_char_replace.pack(side=tk.LEFT)
         self.split = tk.BooleanVar()
-        bn_split = tk.Checkbutton(Fr2,
-            text='splitted numbers', var=self.split)
+        bn_split = tk.Checkbutton(fr2,
+                                  text='splitted numbers',
+                                  var=self.split)
         bn_split.select()
-        bn_split.pack(side=tk.LEFT)  
+        bn_split.pack(side=tk.LEFT)
         self.hyphens = tk.BooleanVar()
-        bn_hyphens = tk.Checkbutton(Fr2,
-            text='hyphenations', var=self.hyphens)
+        bn_hyphens = tk.Checkbutton(fr2,
+                                    text='hyphenations',
+                                    var=self.hyphens)
         bn_hyphens.select()
-        bn_hyphens.pack(side=tk.LEFT)  
+        bn_hyphens.pack(side=tk.LEFT)
         self.html_tags = tk.BooleanVar()
-        bn_html_tags = tk.Checkbutton(Fr2,
-            text='html tags', var=self.html_tags)
+        bn_html_tags = tk.Checkbutton(fr2,
+                                      text='html tags',
+                                      var=self.html_tags)
         bn_html_tags.select()
         bn_html_tags.pack(side=tk.LEFT)
         self.parity_marks = tk.BooleanVar()
-        bn_parity_marks = tk.Checkbutton(Fr2,
-            text='parity marks', var=self.parity_marks)
+        bn_parity_marks = tk.Checkbutton(fr2,
+                                         text='parity marks',
+                                         var=self.parity_marks)
         bn_parity_marks.select()
         bn_parity_marks.pack(side=tk.LEFT)
         self.dashs = tk.BooleanVar()
-        bn_dashs = tk.Checkbutton(Fr2,
-            text='dashes', var=self.dashs)
+        bn_dashs = tk.Checkbutton(fr2,
+                                  text='dashes',
+                                  var=self.dashs)
         bn_dashs.select()
         bn_dashs.pack(side=tk.LEFT)
         self.footnotes = tk.BooleanVar()
-        bn_footnotes = tk.Checkbutton(Fr2,
-            text='footnotes', var=self.footnotes)
+        bn_footnotes = tk.Checkbutton(fr2,
+                                      text='footnotes',
+                                      var=self.footnotes)
         bn_footnotes.select()
         bn_footnotes.pack(side=tk.LEFT)
 
@@ -102,7 +124,8 @@ class ViewCleaning():
         self.result = ScrolledText(parent, bg="black", fg="orange")
         self.result.pack(fill=tk.X)
 
-    def _t_Action(self):
+    def t_action(self):
+        """execute cleaning"""
         self.query = None
         self.result.delete(1.0, "end")
         self.result.insert("end", "Listing text files\n")
@@ -110,19 +133,20 @@ class ViewCleaning():
 
         self._thread = Thread(target=self.list_txt)
         self._thread.start()
-        while(self.query == None):
+        while not self.query:
             self.parent.update()
         self._thread = None
 
         if len(self.query) > 0:
-            self.result.insert("end", u"%d file(s) found\n"%len(self.query))
-            if (self.test.get()):
-                self.result.insert("end", u"only testing\n")
+            self.result.insert("end",
+                               "%d file(s) found\n"%len(self.query))
+            if self.test.get():
+                self.result.insert("end", "only testing\n")
             else:
                 self.result.insert("end",
-                    u"Processing text cleaning\n")
+                                   "Processing text cleaning\n")
             self.progressbar['mode'] = 'determinate'
-            self.progressbar['maximum'] =  len(self.query)
+            self.progressbar['maximum'] = len(self.query)
             n = 0
             for c, txt in enumerate(self.query):
                 n += self.clean_txt(txt)
@@ -130,11 +154,10 @@ class ViewCleaning():
                 self.parent.update()
                 self.result.see("end")
 
-            self.result.insert("end", u"%d file(s) cleaned\n"%n)
+            self.result.insert("end", "%d file(s) cleaned\n"%n)
 
         else:
-            self.result.insert("end", u"Nothing found\n")
-            
+            self.result.insert("end", "Nothing found\n")
 
     def clean_txt(self, txt):
         with open(txt, 'rb') as f:
@@ -142,8 +165,8 @@ class ViewCleaning():
 
         options = ""
         if self.utf.get():
-            options += "u"      
-        if self.ascii.get() :
+            options += "u"
+        if self.ascii.get():
             options += "a"
         if self.char_replace.get():
             options += "c"
@@ -152,7 +175,7 @@ class ViewCleaning():
         if self.hyphens.get():
             options += "h"
         if self.html_tags.get():
-            options += "t"      
+            options += "t"
         if self.parity_marks.get():
             options += "p"
         if self.dashs.get():
@@ -163,40 +186,37 @@ class ViewCleaning():
         c = Cleaner(b, options)
 
         if not set([x for x in c.log.values()]) == set([0]):
-            """if something has to be corrected"""
+            #if something has to be corrected
             self.result.insert("end", "%s  "%txt)
             self.result.insert("end",
-                "%s\n"%"; ".join(["%s: %d"%(x, y)
-                                  for x, y in c.log.items() if y != 0]))
-            if (not self.test.get()):
-                """if not test mode"""
-                buf = bytes(c.content, 'latin-1')   
+                               "%s\n"%"; ".join(["%s: %d"%(x, y)\
+                               for x, y in c.log.items() if y != 0]))
+            if not self.test.get():
+                #if not test mode
+                buf = bytes(c.content, 'latin-1')
                 with open(txt, 'wb') as f:
                     f.write(buf)
             return 1
         else:
             return 0
-                   
 
     def list_txt(self):
         self.progressbar['mode'] = 'indeterminate'
         self.progressbar['maximum'] = 100
         self.progressbar.start(50)
         self.parent.update()
-        rep = self.choosenDir.get()
+        rep = self.choosendir.get()
         if rep == '':
             self.result.insert("end", "No directory selected")
         else:
             self.query = list_files(rep=rep,
-                    recursive=self.Recursive.get())
+                                    recursive=self.is_recursive.get())
         self.progressbar.stop()
 
-        
     def sel_dir(self):
-        self.choosenDir.set("")
+        self.choosendir.set("")
         self.result.delete(1.0, "end")
         self.progressbar['value'] = 0
-        dir = filedialog.askdirectory(title=u"Choose directory",
-            initialdir="C:\corpus")
-        self.choosenDir.set(dir)
-
+        rep = filedialog.askdirectory(title="Choose directory",
+                                      initialdir=r"C:\corpus")
+        self.choosendir.set(rep)
