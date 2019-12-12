c256de1907ba067c4461b504be149f91a55ce556
josquindebaz <josquindebaz@pm.me>
2019-12-12 12:18:08 +0100

    for windows xp

---
+++ EXEC/makeP34.bat
@@ -0,0 +1,11 @@
+del Tiresias.zip
+mkdir Tiresias
+"C:\Python34\Scripts\pyinstaller.exe"  -y --clean --onefile --distpath Tiresias "..\Tiresias.py"
+rd /S /Q build
+xcopy "..\README.md" Tiresias /Y /D /K 
+xcopy "..\CHANGELOG.txt" Tiresias /Y /D /K  
+xcopy "..\param.json" Tiresias /Y /D /K  
+xcopy "..\data" "Tiresias\data\" /Y /E /D /I /K   
+"c:\Program Files\7-Zip\7z.exe" u TiresiasP34.zip Tiresias
+rd /S /Q Tiresias
+del Tiresias.spec
