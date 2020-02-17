9c2928ef47102a347bfb18a0311ed7bc0fcac012
Josquin Debaz <josquin.debaz@gmail.com>
2020-02-17 12:25:15 +0100

    correct bug pyinstaller p38

--- EXEC/makeP34.bat
+++ EXEC/makeP34.bat
@@ -5,6 +5,7 @@ rd /S /Q build
 xcopy "..\README.md" Tiresias /Y /D /K 
 xcopy "..\CHANGELOG.txt" Tiresias /Y /D /K  
 xcopy "..\param.json" Tiresias /Y /D /K  
+xcopy "..\frm" "Tiresias\data\" /Y /E /D /I /K  
 xcopy "..\data" "Tiresias\data\" /Y /E /D /I /K   
 "c:\Program Files\7-Zip\7z.exe" u TiresiasP34.zip Tiresias
 rd /S /Q Tiresias
